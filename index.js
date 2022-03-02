d22ecb6a875c6fb523bd9a4d1fc9d0aff6e40352
Tassnim Alkusaimi <talkusaimi@gmail.com>
2022-03-02 00:58:41 -0500

    commit tree 23997b16607175dd0056795e41276200aab1c25b

---
+++ index.js
@@ -0,0 +1,58 @@
+function add() {
+  console.log(1 + 80);
+}
+
+function subtract(){
+  console.log(60 - 40);
+}
+
+function multiply() {
+  console.log(2 * 3.4); //=> 6.8
+}
+function divide() {
+  console.log(5.0 / 2.5);
+}  
+
+function add(a, b) {
+  return (a+b);
+}
+
+function subtract(a, b){
+  return (a-b);
+}
+
+function multiply(a, b) {
+  return (a*b);
+}
+
+function divide(a, b) {
+  return (a/b);
+}  
+
+function increment(number) {
+  return (++number);
+}
+
+function decrement(number) {
+  return (--number);
+}
+
+function makeInt(string){
+  return parseInt(string);
+ }
+
+ function preserveDecimal(string) {
+    return parseInt(radix);
+  }
+
+  function makeInt(n){
+    return toString();
+   }
+
+function preserveDecimal(n) {
+    return parseInt(`2.222`);
+}
+
+function preserveDecimal(n) {
+  return toString();
+}
